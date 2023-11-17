7ab66cb8641ad8a3c3685eabcff63881dadcd5e3
Nerdy Gedoni <gedoniani@gmail.com>
2023-11-17 16:24:08 +0100

    Home features and category added

---
+++ src/components/Footer.vue
@@ -0,0 +1,85 @@
+<template>
+    <!-- footer -->
+    <footer class="bg-white pt-16 pb-12 border-t border-gray-100">
+        <div class="container grid grid-cols-1 ">
+            <div class="col-span-1 space-y-4">
+                <img src="../assets/images/logo-1.svg" alt="logo" class="w-30">
+                <div class="mr-2">
+                    <p class="text-gray-700 mb-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, hic?
+                    </p>
+                </div>
+                
+            </div>
+
+            <div class="col-span-2 md:grid grid-cols-2 gap-4 mt-4 sm:flex sm:flex-col sm:gap-4 md:flex-row lg:flex-row xl:flex-row">
+                <div class="grid grid-cols-2 gap-8 md:gap-8">
+                    <div>
+                        <h3 class="text-sm font-semibold text-gray-700 uppercase tracking-wider">Solutions</h3>
+                        <div class="mt-4 space-y-4">
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Home</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">About</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Commerce</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Insights</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Contact</a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 class="text-sm font-semibold text-gray-700 uppercase tracking-wider">Support</h3>
+                        <div class="mt-4 space-y-4">
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Home</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">About</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Commerce</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Insights</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Contact</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-2 gap-8 md:gap-8">
+                    <div>
+                        <h3 class="text-sm font-semibold text-gray-700 uppercase tracking-wider">Solutions</h3>
+                        <div class="mt-4 space-y-4">
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Home</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">About</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Commerce</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Insights</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Contact</a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 class="text-sm font-semibold text-gray-700 uppercase tracking-wider">Support</h3>
+                        <div class="mt-4 space-y-4">
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Home</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">About</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Commerce</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Insights</a>
+                            <a href="#" class="text-base text-gray-600 hover:text-gray-900 block">Contact</a>
+                        </div>
+                    </div>
+                </div>
+    
+            </div>
+        </div>
+    </footer>
+    <!-- ./footer -->
+
+    <!-- copyright -->
+    <div class="bg-gray-800 py-4">
+        <div class="container flex items-center justify-between">
+            <p class="text-white">&copy; 2023 Nerdy </p>
+            <div>
+                <div class="flex space-x-5">
+                    <a href="#" class="text-gray-400 hover:text-gray-500"><i
+                            class="fa-brands fa-facebook-square"></i></a>
+                    <a href="#" class="text-gray-400 hover:text-gray-500"><i
+                            class="fa-brands fa-instagram-square"></i></a>
+                    <a href="#" class="text-gray-400 hover:text-gray-500"><i
+                            class="fa-brands fa-twitter-square"></i></a>                   
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -20,21 +20,95 @@
     </div>
     <!-- ./banner -->
 
+    <!-- features -->
+    <div class="container py-16">
+        <div class="w-10/12 grid grid-cols-1 md:grid-cols-3 gap-6 mx-auto justify-center">
+            <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
+                <img src="../assets/images/icons/delivery-van.svg" alt="Delivery" class="w-12 h-12 object-contain">
+                <div>
+                    <h4 class="font-medium capitalize text-lg">Free Shipping</h4>
+                    <p class="text-gray-500 text-sm">Order over $200</p>
+                </div>
+            </div>
+            <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
+                <img src="../assets/images/icons/money-back.svg" alt="Delivery" class="w-12 h-12 object-contain">
+                <div>
+                    <h4 class="font-medium capitalize text-lg">Money Returns</h4>
+                    <p class="text-gray-500 text-sm">30 days money returs</p>
+                </div>
+            </div>
+            <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
+                <img src="../assets/images/icons/service-hours.svg" alt="Delivery" class="w-12 h-12 object-contain">
+                <div>
+                    <h4 class="font-medium capitalize text-lg">24/7 Support</h4>
+                    <p class="text-gray-500 text-sm">Customer support</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ./features -->
+
+    <!-- categories -->
+    <div class="container py-16">
+        <h2 class="text-2xl font-semibold text-gray-700 uppercase mb-6">shop by category</h2>
+        <div class="grid grid-cols-3 gap-3">
+            <div class="relative rounded-sm overflow-hidden group">
+                <img src="../assets/images/category/2.jpg" alt="category 1" class="w-full">
+                <a href="#"
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Bedroom</a>
+            </div>
+            <div class="relative rounded-sm overflow-hidden group">
+                <img src="../assets/images/category/2.jpg" alt="category 1" class="w-full">
+                <a href="#"
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Mattrass</a>
+            </div>
+            <div class="relative rounded-sm overflow-hidden group">
+                <img src="../assets/images/category/2.jpg" alt="category 1" class="w-full">
+                <a href="#"
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Outdoor
+                </a>
+            </div>
+            <div class="relative rounded-sm overflow-hidden group">
+                <img src="../assets/images/category/2.jpg" alt="category 1" class="w-full">
+                <a href="#"
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Sofa</a>
+            </div>
+            <div class="relative rounded-sm overflow-hidden group">
+                <img src="../assets/images/category/2.jpg" alt="category 1" class="w-full">
+                <a href="#"
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Living
+                    Room</a>
+            </div>
+            <div class="relative rounded-sm overflow-hidden group">
+                <img src="../assets/images/category/2.jpg" alt="category 1" class="w-full">
+                <a href="#"
+                    class="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">Kitchen</a>
+            </div>
+        </div>
+    </div>
+    <!-- ./categories -->
+
+     <!-- product -->
+     <div class="container pb-16">
+        <h2 class="text-2xl font-semibold text-gray-700 uppercase mb-6">Products</h2>
+    </div>
+     <!-- ./product -->
+
+     <!-- footer -->
+     <Footer/>
+     <!-- end footer -->
+
 </template>
 
 <script>
 import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
   components: {
-    Header,   
+    Header, 
+    Footer  
   },
 
-  data() {
-    return {
-      imageUrl: 'assets/images/banner-bg.jpg',
-    };
-  },
-   
 };
 </script>
