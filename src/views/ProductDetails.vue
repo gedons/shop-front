42b490a9825e6b0a4bb4e077300a915194602588
Nerdy Gedoni <gedoniani@gmail.com>
2023-12-02 17:23:48 +0100

    product details updated

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -90,7 +90,7 @@
 
         <div>
             <h2 class="text-3xl font-medium uppercase mb-2">{{productDetails.title}}</h2>
-            <div class="flex items-center mb-4">
+            <!-- <div class="flex items-center mb-4">
                 <div class="flex gap-1 text-sm text-yellow-400">
                     <span><i class="fa-solid fa-star"></i></span>
                     <span><i class="fa-solid fa-star"></i></span>
@@ -99,7 +99,7 @@
                     <span><i class="fa-solid fa-star"></i></span>
                 </div>
                 <div class="text-xs text-gray-500 ml-3">(150 Reviews)</div>
-            </div>
+            </div> -->
             <div class="space-y-2">
                 <p class="text-gray-800 font-semibold space-x-2">
                     <span>Availability: </span>
